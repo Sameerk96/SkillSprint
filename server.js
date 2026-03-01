9af1538fd3001d1e69a56a98869f1b9a95e69b23
Sameerk96 <sameer96277@gmail.com>
2026-03-01 12:07:44 +0530

    commit

---
+++ server.js
@@ -0,0 +1,243 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+/* MongoDB Connection */
+
+mongoose.connect("mongodb://127.0.0.1:27017/skillsprint")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+
+/* User Model */
+
+const User = mongoose.model("User",{
+    name:String,
+    email:String,
+    password:String,
+    role:String
+});
+
+
+/* Signup */
+
+app.post("/signup", async(req,res)=>{
+
+    const user = new User({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        role:"user"
+    });
+
+    await user.save();
+
+    res.send("User Created");
+
+});
+
+
+/* Login */
+
+app.post("/login", async(req,res)=>{
+
+const user = await User.findOne({
+    email:req.body.email,
+    password:req.body.password
+});
+
+if(user){
+
+    if(user.role=="admin")
+        res.send("admin");
+
+    else
+        res.send("user");
+
+}
+else{
+res.send("not found");
+}
+
+});
+
+
+/* Get All Users (Admin) */
+
+app.get("/users", async(req,res)=>{
+
+const users = await User.find();
+
+res.json(users);
+
+});
+/* SAVE FIREBASE USERS */
+
+app.post("/saveUser", async(req,res)=>{
+
+const exists = await User.findOne({
+email:req.body.email
+});
+
+if(!exists){
+
+const user = new User({
+
+name:req.body.name,
+email:req.body.email,
+password:"",
+role:"user"
+
+});
+
+await user.save();
+
+console.log("Firebase user saved");
+
+}
+
+res.send("saved");
+
+});
+/* COURSE MODEL */
+
+const Course = mongoose.model("Course",{
+
+name:String,
+image:String,
+description:String
+
+});
+
+/* VIDEO MODEL */
+
+const Video = mongoose.model("Video",{
+
+course:String,
+title:String,
+videoId:String
+
+});
+
+
+/* ADD COURSE */
+
+app.post("/addCourse",async(req,res)=>{
+
+const course = new Course({
+
+name:req.body.name,
+image:req.body.image,
+description:req.body.description
+
+});
+
+await course.save();
+
+res.send("Course Added");
+
+});
+
+
+/* GET COURSES */
+
+app.get("/courses",async(req,res)=>{
+
+const courses = await Course.find();
+
+res.json(courses);
+
+});
+
+
+/* ADD VIDEO */
+
+app.post("/addVideo",async(req,res)=>{
+
+const video = new Video({
+
+course:req.body.course,
+title:req.body.title,
+videoId:req.body.videoId
+
+});
+
+await video.save();
+
+res.send("Video Added");
+
+});
+
+
+/* GET VIDEOS */
+
+app.get("/videos/:course",async(req,res)=>{
+
+const videos = await Video.find({
+
+course:req.params.course
+
+});
+
+res.json(videos);
+
+});
+
+
+
+/* DELETE COURSE */
+
+app.delete("/deleteCourse/:name", async(req,res)=>{
+
+await Course.deleteOne({
+
+name:req.params.name
+
+});
+
+await Video.deleteMany({
+
+course:req.params.name
+
+});
+
+res.send("Course Deleted");
+
+});
+
+
+/* DELETE VIDEO */
+
+app.delete("/deleteVideo/:id", async(req,res)=>{
+
+await Video.deleteOne({
+
+_id:req.params.id
+
+});
+
+res.send("Video Deleted");
+
+});
+
+
+/* GET ALL VIDEOS */
+
+app.get("/allVideos", async(req,res)=>{
+
+const videos = await Video.find();
+
+res.json(videos);
+
+});
+
+
+app.listen(3000,()=>{
+console.log("Server Running");
+});
